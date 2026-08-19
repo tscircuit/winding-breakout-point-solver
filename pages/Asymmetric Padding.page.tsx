0e719e259157c3bb29d63d9a71d1bc1f78ec3f78
ankan roy <ankan@ankans-MacBook-Air.local>
2026-08-20 04:16:02 +0530

    flatten asymmetric padding Cosmos fixture

---
+++ pages/Asymmetric Padding.page.tsx
@@ -0,0 +1,6 @@
+import { createAsymmetricPaddingExample } from "../examples/padding/asymmetric-padding-example"
+import { WindingBreakoutSolverFixture } from "./am62l-lpddr4-ram/WindingBreakoutSolverFixture"
+
+export default (
+  <WindingBreakoutSolverFixture loadExample={createAsymmetricPaddingExample} />
+)
